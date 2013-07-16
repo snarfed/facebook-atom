060a8abc2db53cc7515a8f2b4f946ecb486e425c
Ryan Barrett <git@ryanb.org>
2013-07-16 15:35:21 -0700

    canonicalize host_url

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ class AtomHandler(webapp2.RequestHandler):
          'updated': activities[0]['object'].get('updated') if activities else '',
          'actor': actor,
          'items': activities,
-         'host_url': self.request.host_url,
+         'host_url': self.request.host_url + "/",
          'request_url': self.request.url,
          }))
 
