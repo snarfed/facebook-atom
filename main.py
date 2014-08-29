62efb1ea4e8bde4cd06dcb72b77cf101c199ab32
Ryan Barrett <git@ryanb.org>
2014-08-29 08:30:31 -0700

    generate notification inside facebook when a user's access token expires

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from activitystreams import appengine_config
 from activitystreams import facebook
 from activitystreams.oauth_dropins import facebook as oauth_facebook
 from activitystreams.oauth_dropins.webutil import handlers
+from activitystreams.oauth_dropins.webutil import util
 import webapp2
 
 from google.appengine.ext.webapp import template
@@ -31,8 +32,8 @@ API_HOME_URL = ('https://graph.facebook.com/me?access_token=%s&fields='
 class CallbackHandler(oauth_facebook.CallbackHandler):
   def finish(self, auth_entity, state=None):
     """Gets an access token based on an auth code."""
-    atom_url = '%s/atom?access_token=%s' % (self.request.host_url,
-                                            auth_entity.access_token())
+    atom_url = '%s/atom?user_id=%s&access_token=%s' % (
+      self.request.host_url, auth_entity.key.id(), auth_entity.access_token())
     logging.info('generated feed URL: %s', atom_url)
     self.response.out.write(template.render(GENERATED_TEMPLATE_FILE,
                                             {'atom_url': atom_url}))
@@ -50,11 +51,41 @@ class AtomHandler(webapp2.RequestHandler):
   handle_exception = handlers.handle_exception
 
   def get(self):
-    access_token = self.request.get('access_token')
-    assert access_token
-    resp = json.loads(urllib2.urlopen(API_HOME_URL % access_token, timeout=999).read())
-
+    host_url = self.request.host_url + "/"
+    access_token = util.get_required_param(self, 'access_token')
+    user_id = self.request.get('user_id')
     fb = facebook.Facebook(self)
+
+    try:
+      resp = urllib2.urlopen(API_HOME_URL % access_token, timeout=999)
+      resp = json.loads(resp.read())
+    except urllib2.HTTPError, e:
+      # Facebook API error details:
+      # https://developers.facebook.com/docs/graph-api/using-graph-api/#receiving-errorcodes
+      # https://developers.facebook.com/docs/reference/api/errors/
+      body = e.read()
+      try:
+        error = json.loads(body).get('error', {})
+      except:
+        # response isn't JSON
+        error = {}
+      if (user_id and
+          # bad access token, oauth error
+          error.get('code') in (102, 190) and
+          # revoked, expired, changed password
+          error.get('error_subcode') in (458, 463, 460)):
+        fb.create_notification(
+          user_id,
+          "Your Facebook Atom feed's access has expired. Click here to renew it!",
+          host_url)
+        self.response.set_status(403)
+        self.response.write(
+          'Your Facebook access has expired. Go to %s to renew it!' % host_url)
+      else:
+        self.response.set_status(e.code)
+        self.response.write(body)
+      return
+
     actor = fb.user_to_actor(resp)
     posts = resp.get('home', {}).get('data', [])
     activities = [fb.post_to_activity(p) for p in posts]
