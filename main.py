aeedd1197b20bb7cf710e4ed5df876d51502c968
Ryan Barrett <git@ryanb.org>
2014-10-07 20:21:19 -0700

    port to activitystreams-unofficial and get_activities()

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os
 import urllib2
 
 from activitystreams import appengine_config
+from activitystreams import atom
 from activitystreams import facebook
 from activitystreams.oauth_dropins import facebook as oauth_facebook
 from activitystreams.oauth_dropins.webutil import handlers
@@ -18,16 +19,6 @@ import webapp2
 
 from google.appengine.ext.webapp import template
 
-# https://developers.facebook.com/docs/reference/login/
-OAUTH_SCOPES = facebook.OAUTH_SCOPES + ',offline_access'
-GENERATED_TEMPLATE_FILE = os.path.join(os.path.dirname(__file__),
-                                       'templates', 'generated.html')
-ATOM_TEMPLATE_FILE = os.path.join(os.path.dirname(__file__),
-                                  'activitystreams', 'templates', 'user_feed.atom')
-
-API_HOME_URL = ('https://graph.facebook.com/me?access_token=%s&fields='
-                'home.limit(50),id,username,name,link,updated_time,bio,location')
-
 
 class CallbackHandler(oauth_facebook.CallbackHandler):
   def finish(self, auth_entity, state=None):
@@ -35,8 +26,9 @@ class CallbackHandler(oauth_facebook.CallbackHandler):
     atom_url = '%s/atom?user_id=%s&access_token=%s' % (
       self.request.host_url, auth_entity.key.id(), auth_entity.access_token())
     logging.info('generated feed URL: %s', atom_url)
-    self.response.out.write(template.render(GENERATED_TEMPLATE_FILE,
-                                            {'atom_url': atom_url}))
+    self.response.out.write(template.render(
+        os.path.join(os.path.dirname(__file__), 'templates', 'generated.html'),
+        {'atom_url': atom_url}))
 
 
 def actor_name(actor):
@@ -52,13 +44,10 @@ class AtomHandler(webapp2.RequestHandler):
 
   def get(self):
     host_url = self.request.host_url + "/"
-    access_token = util.get_required_param(self, 'access_token')
-    user_id = self.request.get('user_id')
-    fb = facebook.Facebook(self)
+    fb = facebook.Facebook(access_token=util.get_required_param(self, 'access_token'))
 
     try:
-      resp = urllib2.urlopen(API_HOME_URL % access_token, timeout=999)
-      resp = json.loads(resp.read())
+      activities = fb.get_activities()
     except urllib2.HTTPError, e:
       # Facebook API error details:
       # https://developers.facebook.com/docs/graph-api/using-graph-api/#receiving-errorcodes
@@ -69,6 +58,8 @@ class AtomHandler(webapp2.RequestHandler):
       except:
         # response isn't JSON
         error = {}
+
+      user_id = self.request.get('user_id')
       if (user_id and
           # bad access token, oauth error
           error.get('code') in (102, 190) and
@@ -86,10 +77,6 @@ class AtomHandler(webapp2.RequestHandler):
         self.response.write(body)
       return
 
-    actor = fb.user_to_actor(resp)
-    posts = resp.get('home', {}).get('data', [])
-    activities = [fb.post_to_activity(p) for p in posts]
-
     # massage data
     for a in activities:
       obj = a.setdefault('object', {})
@@ -97,20 +84,16 @@ class AtomHandler(webapp2.RequestHandler):
       if 'content' not in obj and obj['objectType'] == 'image':
         obj['content'] = '%s added a new photo.' % actor_name(who)
 
-    self.response.headers['Content-Type'] = 'text/xml'
-    self.response.out.write(template.render(
-        ATOM_TEMPLATE_FILE,
-        {'title': 'Facebook news feed for %s' % actor_name(actor),
-         'updated': activities[0]['object'].get('updated') if activities else '',
-         'actor': actor,
-         'items': activities,
-         'host_url': self.request.host_url + "/",
-         'request_url': self.request.url,
-         }))
+    self.response.headers['Content-Type'] = 'application/atom+xml'
+    self.response.out.write(atom.activities_to_atom(
+        activities, fb.get_actor(), host_url=host_url,
+        request_url=self.request.path_url))
 
 
 application = webapp2.WSGIApplication(
-  [('/generate', oauth_facebook.StartHandler.to('/got_auth_code', OAUTH_SCOPES)),
+  [('/generate', oauth_facebook.StartHandler.to('/got_auth_code',
+        # https://developers.facebook.com/docs/reference/login/
+        facebook.OAUTH_SCOPES + ',offline_access')),
    ('/got_auth_code', CallbackHandler),
    ('/atom', AtomHandler),
    ], debug=appengine_config.DEBUG)
