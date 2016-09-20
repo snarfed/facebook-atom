1e6d1761d8d09b01c371f25339b7f45c865a8c90
Ryan Barrett <git@ryanb.org>
2016-09-20 13:25:39 -0700

    blacklist "is going to an event" stories

--- cookie.py
+++ cookie.py
@@ -50,8 +50,8 @@ OMIT_URL_PARAMS = {'bacr', 'ext', '_ft_', 'hash', 'refid'}
 # the double spaces are intentional. it's how FB renders these stories. should
 # help prevent false positives.
 BLACKLIST = frozenset([
-  re.compile(r'  (are now friends|is now friends with|(also )?commented on|like[ds]|reacted to|replied to|followed)(  | this| a)'),
-  re.compile(r'  ((was|were) (mentioned|tagged) in( a)?|>)  '),
+  re.compile(r'  (are now friends|is now friends with|(also )?commented on|like[ds]|reacted to|replied to|followed|is going to)(  ?| this| an?)'),
+  re.compile(r'  ((was|were) (mentioned|tagged) in( an?)?|>)  '),
   re.compile(r"  (wrote on|shared a  .+  to)  .+ 's (wall|timeline)", re.I),
   re.compile(r' Add Friend$', re.I),
   re.compile(r'Suggested Post', re.I),
